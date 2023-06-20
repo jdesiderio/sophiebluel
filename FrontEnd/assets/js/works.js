25ee9eeb7118779d0a752c3b18fccb196c105875
jdesiderio <desiderio.janyce@gmail.com>
2023-06-20 17:18:56 +0200

    clean up works.js + css buttons

--- FrontEnd/assets/js/works.js
+++ FrontEnd/assets/js/works.js
@@ -2,106 +2,83 @@
 const works = await response.json();
 console.log(works);*/
 
+const root = "http://localhost:5678/api/";
+
 // Récupération des fiches
-fetch('http://localhost:5678/api/works', { method: 'GET' })
+function loadWorks() {
+  fetch(root + "works", { method: 'GET' })
   .then((response) => response.json())
   .then((json) => { 
-    let works = json;
-
-    // Générer le portfolio
-    function generateWorks(works){
-      for (let i = 0; i < works.length; i++) {
-          const figure = works[i];
-          // Récupération de l'élément du DOM qui accueillera les fiches
-          const gallery = document.querySelector(".gallery");
-          // Création d’une balise dédiée à une fiche
-          const workElement = document.createElement("figure");
-          // Création des balises 
-          const imageElement = document.createElement("img");
-          imageElement.src = figure.imageUrl;
-          const titleElement = document.createElement("figcaption");
-          titleElement.innerText = figure.title;
-          
-          // On rattache la balise figure au div gallery 
-          gallery.appendChild(workElement);
-          workElement.appendChild(imageElement);
-          workElement.appendChild(titleElement);
-       }
-    };
+    works = json;
+    // console.log(works);
     generateWorks(works);
+  });
+};
+loadWorks();
 
-    // Création des boutons de filtres
-    const category = works.map(works => works.category);
-    console.log(category);
-    // récupérer les catégories uniques
-    const distinctCat = [];
-    const map = new Map();
-    for (const item of category) {
-        if(!map.has(item.id)){
-            map.set(item.id, true);
-            distinctCat.push({
-                id: item.id,
-                name: item.name
-            });
-        }
-    };
-    console.log(distinctCat);
-    
-    // générer les boutons de filtres
-    function generateBtns(distinctCat) {
-      const filters = document.querySelector(".filters");
-      const btnElementAll = document.createElement("button");
-      btnElementAll.classList.add("button");
-      btnElementAll.setAttribute("id", "0");
-      btnElementAll.innerText = "Tous";
-      filters.appendChild(btnElementAll);
+// Générer les fiches du portfolio
+function generateWorks(works){
+  works.forEach(work => {
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const gallery = document.querySelector(".gallery");
+    // Création d’une balise dédiée à une fiche
+    const workElement = document.createElement("figure");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = work.imageUrl;
+    const titleElement = document.createElement("figcaption");
+    titleElement.innerText = work.title;
+    // On rattache la balise figure au div gallery 
+    gallery.appendChild(workElement);
+    workElement.appendChild(imageElement);
+    workElement.appendChild(titleElement);
+  });
+};
 
-      for (let j = 0; j < distinctCat.length; j++) {
-      const btn = distinctCat[j];
-      const btnElement = document.createElement("button");
-      btnElement.classList.add("button");
-      btnElement.setAttribute("id", j+1);
-      btnElement.innerText = btn.name;
-      filters.appendChild(btnElement);
-      };
-    };
-    generateBtns(distinctCat);
+function loadCat() {
+  fetch(root + "categories", { method: 'GET' })
+  .then((response) => response.json())
+  .then((json) => { 
+    categories = json;
+    console.log(categories);
+    generateBtns(categories);
 
-// Ajout du listener pour filtrer les pièces non abordables
-/*const boutonFiltrer = document.querySelector(".btn-filtrer");
-boutonFiltrer.addEventListener("click", function () {
-   const piecesFiltrees = pieces.filter(function (piece) {
-       return piece.disponibilite;
-   });
-   // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
-  document.querySelector(".fiches").innerHTML = "";
-  genererPieces(piecesFiltrees);
-});*/
+    // Ajout du listener sur chaque bouton filtre
+    let filterBtn = document.querySelectorAll(".button");
+    filterBtn.forEach(btn => {
+      btn.addEventListener("click", filterWorks);
+    });
+    //console.log(filterBtn);
+  });
+};
+loadCat();
 
-  // Ajout du listener sur chaque bouton filtre
-  let filterBtn = document.querySelectorAll(".button");
-  for (let j = 0; j < filterBtn.length; j++) {
-    filterBtn[j].addEventListener("click", filterWorks);
-    //console.log(filterBtn[j]);
-  }
+// générer les boutons de filtres
+function generateBtns(categories) {
+  const filters = document.querySelector(".filters");
+  const btnElementAll = document.createElement("button");
+  btnElementAll.classList.add("button");
+  // btnElementAll.setAttribute("id", "0");
+  btnElementAll.innerText = "Tous";
+  filters.appendChild(btnElementAll);
 
-  // fonction pour filtrer
-  function filterWorks(event) {
-    const btnId = parseInt(event.target.id);
-    console.log(btnId);
-    let filteredWorks;
-  
-    if (btnId === 0) {
-      // Cliquer Bouton "Tous" > afficher tous les éléments
-      filteredWorks = works;
-    } else {
-      // Filtrer les éléments en fonction de l'ID de la catégorie du bouton cliqué
-      filteredWorks = works.filter(work => work.category.id === btnId);
-    };
-      console.log(filteredWorks);
-         // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
-      document.querySelector(".gallery").innerHTML = "";
-      generateWorks(filteredWorks);
+  for (let j = 0; j < categories.length; j++) {
+  const btn = categories[j];
+  const btnElement = document.createElement("button");
+  btnElement.classList.add("button");
+  btnElement.setAttribute("id", j+1);
+  btnElement.innerText = btn.name;
+  filters.appendChild(btnElement);
   };
+};
 
-})
+// fonction pour filtrer
+function filterWorks(event) {
+  // Filtrer les éléments
+  let filteredWorks = works.filter(work => event.target.id === "" || work.category.id === parseInt(event.target.id));
+  //console.log(filteredWorks);
+  
+  // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+  document.querySelector(".gallery").innerHTML = "";
+  generateWorks(filteredWorks);
+};
